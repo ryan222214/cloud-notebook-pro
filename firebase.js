4a1ad2bd1869a210a66f14e96ac7f037d68c1510
ryan222214 <148895188+ryan222214@users.noreply.github.com>
2025-11-29 17:39:43 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,8 @@
-// Firebase configuration will go here
+const firebaseConfig = {
+  apiKey: "AIza...",
+  authDomain: "cloud-notebook-pro.firebaseapp.com",
+  projectId: "cloud-notebook-pro",
+  storageBucket: "cloud-notebook-pro.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef..."
+};
