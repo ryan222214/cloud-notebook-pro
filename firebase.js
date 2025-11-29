adf363b6fa2da07167b2de7c2a8241abbd2d6e7d
ryan222214 <148895188+ryan222214@users.noreply.github.com>
2025-11-29 17:41:49 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-  apiKey: "AIza...",
+  apiKey: "AIzaSyAc61u9eQg7gVakbhbQq-yI14bY9ECHsOw",
   authDomain: "cloud-notebook-pro.firebaseapp.com",
   projectId: "cloud-notebook-pro",
-  storageBucket: "cloud-notebook-pro.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef..."
+  storageBucket: "cloud-notebook-pro.firebasestorage.app",
+  messagingSenderId: "530994617086",
+  appId: "1:530994617086:web:c270ae41c2a802506823af"
 };
